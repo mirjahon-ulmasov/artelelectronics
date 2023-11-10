520134aa0d2c612ff8e07c68687589018cb1898d
Mirjahon Ulmasov <mirjahonulmasov@gmail.com>
2023-11-10 12:04:58 +0500

    changed

--- src/pages/news/edit.tsx
+++ src/pages/news/edit.tsx
@@ -1,3 +1,12 @@
+import { Button, Result } from "antd";
+
 export default function EditColor() {
-    return <div>EditColor</div>
+    return (
+        <Result
+            status="404"
+            title="В разработке"
+            subTitle="К сожалению, страница, которую вы посетили, еще не существует."
+            extra={<Button type="primary">Back Home</Button>}
+        />
+    )
 }
